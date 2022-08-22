4f300898d3d5be1119bf34b614181391177fa6aa
japarra <j.parra.903@gmail.com>
2022-08-22 14:01:47 -0500

    Se incluye el menu mobile de la aplicación

--- main.js
+++ main.js
@@ -1,8 +1,20 @@
 const menu_email = document.querySelector('.navbar-email');
 const desktopMenu = document.querySelector('.desktop-menu');
+
+const iconMenuMobile    = document.querySelector('.menu');
+const mobileMenu        = document.querySelector('.mobile-menu');
+
+
 menu_email.addEventListener('click', toggleDesktopMenu);
+iconMenuMobile.addEventListener('click', toggleMobileMenu);
+
 
 function toggleDesktopMenu ()
 {
     desktopMenu.classList.toggle('inactive');
+}
+
+function toggleMobileMenu()
+{
+    mobileMenu.classList.toggle('inactive');
 }
