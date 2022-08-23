d86d9615483d15aa1039381880253239d35d85e8
japarra <j.parra.903@gmail.com>
2022-08-23 13:20:53 -0500

    AJustes del proyecto

--- main.js
+++ main.js
@@ -1,25 +1,33 @@
 const menu_email = document.querySelector('.navbar-email');
 const desktopMenu = document.querySelector('.desktop-menu');
 
-const iconMenuMobile    = document.querySelector('.menu');
-const mobileMenu        = document.querySelector('.mobile-menu');
+const iconMenuMobile                = document.querySelector('.menu');
+const mobileMenu                    = document.querySelector('.mobile-menu');
 
-const shoppingCart      = document.querySelector('.navbar-shopping-cart');
-const productDetail     = document.querySelector('.product-detail');
-const cardsContainer    = document.querySelector('.cards-container');
+const shoppingCart                  = document.querySelector('.navbar-shopping-cart');
+const productDetail                 = document.querySelector('#shopping-cart-container');
+const productDetailContainer        = document.querySelector('#product-detail');
+const cardsContainer                = document.querySelector('.cards-container');
+const ProductDetailcloseIcon        = document.querySelector('.product-detail-close');
 
 menu_email.addEventListener('click', toggleDesktopMenu);
 iconMenuMobile.addEventListener('click', toggleMobileMenu);
 shoppingCart.addEventListener('click', toggleProductDetail);
-
+ProductDetailcloseIcon.addEventListener('click', closeProductDetail);
 
 function toggleDesktopMenu ()
 {
-    const isProductDetail  = !productDetail.classList.contains('inactive');
-
+    const isProductDetail           = !productDetail.classList.contains('inactive');
+    const isProductDetailContainer  = !productDetailContainer.classList.contains('inactive');
+   
     if (isProductDetail)
     {
         productDetail.classList.add('inactive');
+    }
+
+    if (isProductDetailContainer)
+    {
+        productDetailContainer.classList.add('inactive');
     }
 
     desktopMenu.classList.toggle('inactive');
@@ -28,10 +36,17 @@ function toggleDesktopMenu ()
 function toggleMobileMenu()
 {
     const isProductDetail  = !productDetail.classList.contains('inactive');
+    const isProductDetailContainer  = !productDetailContainer.classList.contains('inactive');
+   
 
     if (isProductDetail)
     {
         productDetail.classList.add('inactive');
+    }
+
+    if (isProductDetailContainer)
+    {
+        productDetailContainer.classList.add('inactive');
     }
     
 
@@ -42,7 +57,7 @@ function toggleProductDetail()
 {
     const isDesktopMenuOpen = !desktopMenu.classList.contains('inactive');
     const isMobileMenuOpen  = !mobileMenu.classList.contains('inactive');
-    
+    const isProductDetailContainer  = !productDetailContainer.classList.contains('inactive');
 
     if (isDesktopMenuOpen)
     {
@@ -54,9 +69,36 @@ function toggleProductDetail()
         mobileMenu.classList.add('inactive');
     }
     
+    if (isProductDetailContainer)
+    {
+        productDetailContainer.classList.add('inactive');
+    }
+
     productDetail.classList.toggle('inactive');
 }
 
+function openProductDetail()
+{
+    const isProductDetail  = !productDetail.classList.contains('inactive');
+    const isDesktopMenuOpen = !desktopMenu.classList.contains('inactive');
+
+    if (isProductDetail)
+    {
+        productDetail.classList.add('inactive');
+    }
+
+    if (isDesktopMenuOpen)
+    {
+        desktopMenu.classList.add('inactive');
+    }
+
+    productDetailContainer.classList.remove('inactive');
+}
+
+function closeProductDetail()
+{
+    productDetailContainer.classList.add('inactive');
+}
 const productList = [];
 
 productList.push(
@@ -85,6 +127,14 @@ productList.push(
 
 productList.push(
     {
+        name:'bike',
+        price: 23,
+        image: 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940'
+    
+    });
+
+productList.push(
+    {
         name:'Pc',
         price: 250,
         image: 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940'
@@ -98,6 +148,7 @@ for (product of productList)
 
    const img = document.createElement('img');
    img.setAttribute('src', product.image);
+   img.addEventListener('click', openProductDetail)
    
    const productinfo =  document.createElement('div');
    productinfo.classList.add('product-info');
@@ -127,3 +178,4 @@ for (product of productList)
    cardsContainer.appendChild(productCard);
 
 }
+
